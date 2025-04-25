8fb06a5391f487c2d98f75fedb33b12981347962
Chris Lawrence <chris@thecommerce.co>
2025-04-25 14:52:32 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -20,7 +20,7 @@ export default function Chat() {
     <div className="relative min-h-screen bg-[hsl(var(--amp-onyx-10))] px-2 md:px-0">
       <div className="mx-auto flex w-full max-w-3xl flex-col stretch py-8">
         <h1 className="mb-8 text-center text-2xl font-bold text-[hsl(var(--amp-onyx-75))]">
-          AMP Onboarding Assistant
+          AMP AI CSM
         </h1>
 
         <div className="mb-24 flex-1 space-y-6">
